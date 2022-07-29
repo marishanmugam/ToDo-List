8f54c5e93a8f9972ea652043b70693a820a2c702
marishanmugam <mageshg1402@gmail.com>
2022-07-29 21:18:09 +0530

    todo list

--- Todo list.js
+++ Todo list.js
@@ -2,16 +2,19 @@ let myForm = document.getElementById("myForm");
 let data = document.getElementById("inputbox");
 let myList = document.getElementById("mylist");
 
+console.log(data.value);
 myForm.addEventListener("submit", (event) => {
   event.preventDefault();
   add(data.value);
 });
 
 const add = (inputdata) => {
-  if (inputdata == " ") {
+  if (inputdata.length===0) {
     document.getElementById("error").style.display = "block";
+    return
   } else {
-    let temp = `<li class='list' id='list'><input type='checkbox' id='chkbox' class='chkbox' onclick='check(this)'>${inputdata}<img src="delete.png" alt="" class='imgdel' onclick="del(this)"  tooltip='Delete this item'></li>`;
+    document.getElementById("error").style.display = "none";
+    let temp = `<li class='list' id='list'><input type='checkbox' id='chkbox' class='chkbox' onchange='check(this)'>${inputdata}<img src="delete.png" alt="" class='imgdel' onclick="del(this)"  tooltip='Delete this item'></li>`;
     myList.insertAdjacentHTML("beforebegin", temp);
     data.value = "";
     data.focus();
@@ -22,8 +25,11 @@ const del = (de) => {
   de.parentElement.remove();
 };
 
-const check = (e) => {
-  if (e.this.checked) {
-    document.getElementById("list").style.textDecoration = "line-through";
-  } else document.getElementById("list").style.textDecoration = "";
-};
+// const check = (e) => {
+//   if (e.this.checked) {
+//     document.getElementById("list").style.textDecoration = "line-through";
+//   } else document.getElementById("list").style.textDecoration = "";
+// };
+
+let li = document.querySelector('#mylist').child;
+console.log(li)
